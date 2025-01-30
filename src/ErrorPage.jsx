b91bda0b76b235b11e2d8d8e377ac7719c029afb
seowzhixuan199800126 <zhixuan.seow@gmail.com>
2025-01-30 21:25:32 +0800

    Modified: testing purpose

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -8,7 +8,7 @@ export default function ErrorPage() {
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{error.statusText || error.message} 123</i>
       </p>
     </div>
   );
